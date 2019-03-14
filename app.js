fae7c71565750d7c2b2ba5f3dea1dd5e58b0e68c
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-03-14 12:54:27 +0000

    added getNewQuote function

--- app.js
+++ app.js
@@ -3,6 +3,36 @@ $(document).ready(function(){
   var quote, 
       author;
       
-      
+ function getNewQuote(){
+      $.ajax({
+        url: 'https://api.forismatic.com/api/1.0/',
+        jsonp: 'jsonp',
+        dataType: 'jsonp',
+        data: {
+          method: 'getQuote', 
+          lang: 'en',
+          format: 'jsonp'
+        },
+        success: function(res){
+          console.log(quote);
+          quote = res.quoteText;
+          author = res.quoteAuthor;
+          
+          $('.quote').text(quote);
+          
+          if(author){
+            $('#author').text(' - ' +  author);
+          } else {
+            $('#author').text('-   Unknown');
+          }
+         }
+        
+       });
+    }  
+    
+  getNewQuote();
+    
+    
+    
 
 });
